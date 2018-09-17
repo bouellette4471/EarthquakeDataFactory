ed0edaa39be444550466d27cb8d05d4d3beaaeb3
Benjamin <Benjamin@10.159.60.194>
2018-09-17 14:57:20 -0400

    Initial version

---
+++ src/EarthquakeData.java
@@ -0,0 +1,14 @@
+/*
+ *  Westfield State University: CAIS: Program Design II 
+ *  @author Ben Ouellette (bouellette4471@westfield.ma.edu)
+ *  (c) 2018
+ *  Created: Sep 17, 2018 2:06:23 PM
+ */
+
+/**
+ *
+ * @author Ben Ouellette (bouellette4471@westfield.ma.edu)
+ */
+public class EarthquakeData {
+     
+}
---
+++ src/earthquake/Earthquake.java
@@ -0,0 +1,22 @@
+/*
+ *  Westfield State University: CAIS: Program Design II 
+ *  @author Ben Ouellette (bouellette4471@westfield.ma.edu)
+ *  (c) 2018
+ *  Created: Sep 17, 2018 2:05:54 PM
+ */
+package earthquake;
+
+/**
+ *
+ * @author Ben Ouellette (bouellette4471@westfield.ma.edu)
+ */
+public class Earthquake {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
